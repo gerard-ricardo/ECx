69532d2e1113fe9d859413e5221a5c9673637ed5
Gerard Ricardo <g.ricado@aims.gov.au>
2024-03-12 11:39:57 +1000

    update ecx

--- R/ECx.R
+++ R/ECx.R
@@ -1,23 +1,42 @@
 #' Calculate ECx
 #'
-#' Description of what ECx calculation does, including any important details
-#' users should know.
+#' Calculates an ECx from a glm or glmm (lme4). This function allows users to calculate
+#' ECx values using either the top of the response curve or between the top and bottom
+#' of the response curve.
 #'
-#' @param model A model object, typically from `glm`, for which the ECx will
-#'   be calculated.
-#' @return The calculated ECx value.
+#' @param model A model object, typically from `glm` or `glmer` from the `lme4` package.
+#' @param x The percentage effect to calculate the ECx at (default is 50).
+#' @param type The type of ECx calculation: 'from_top' (default), or 'from_top_bot'.
+#' @return A data frame with ECx, lower, and upper confidence limits.
 #' @export
 #' @examples
-#' # Here, you could include a simple example of how to use the ECx function.
+#' # Ensure you have the 'hav4' dataset available before running this example
+#' # md1 <- glm(cbind(suc,(tot - suc)) ~ raw_x, family = binomial, data = hav4)
+#' # ECx(model = md1, x = 50)
+ECx <- function(model, x = 50, type = c('from_top', 'from_top_bot', 'absolute')) {
+  type <- match.arg(type)
 
+  # Check if the model is a GLMM from lme4
+  if("glmerMod" %in% class(model)) {
+    beta <- fixef(model)  # For GLMMs, use fixef() to get fixed effects
+  } else {
+    beta <- coef(model)  # For GLMs, use coef()
+  }
 
+  pred_probs <- predict(model, type = "response")
 
-ECx <- function(model =  model, x = 50) {
-  pred_probs <- predict(md1, type = "response")
-  inhib <- max(pred_probs)-((x/100) * max(pred_probs))
-  library(VGAM)
-  eta <- logitlink(inhib)
-  beta <- coef(model)[1:2]
+  if (type == 'from_top') {
+    inhib <- max(pred_probs) - ((x/100) * max(pred_probs))
+  } else if (type == 'from_top_bot') {
+    range_val <- max(pred_probs) - min(pred_probs)
+    target_val <- min(pred_probs) + (range_val * (x/100))
+    inhib <- target_val
+  } else if (type == 'absolute') {
+    inhib <- x/100
+  }
+
+  eta <- VGAM::logitlink(inhib)
+  #beta <- coef(model)[1:2]
   ECx <- (eta - beta[1]) / beta[2]
   pd <- -cbind(1, ECx) / beta[2]
   ff <- as.matrix(vcov(model)[1:2, 1:2])
@@ -26,4 +45,4 @@ ECx <- function(model =  model, x = 50) {
   lower <- (ECx - se * 1.96)
   sum <- data.frame(ECx, lower, upper)
   return(sum)
-} # using coef
+}
