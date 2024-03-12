2a58194f46273ba88934a52db00215b7d56eb9f1
Gerard Ricardo <g.ricado@aims.gov.au>
2024-03-12 10:27:49 +1000

    fixed ecx

--- R/ECx.R
+++ R/ECx.R
@@ -1,13 +1,29 @@
-ECx <- function(x, y) {
+#' Calculate ECx
+#'
+#' Description of what ECx calculation does, including any important details
+#' users should know.
+#'
+#' @param model A model object, typically from `glm`, for which the ECx will
+#'   be calculated.
+#' @return The calculated ECx value.
+#' @export
+#' @examples
+#' # Here, you could include a simple example of how to use the ECx function.
+
+
+
+ECx <- function(model =  model, x = 50) {
+  pred_probs <- predict(md1, type = "response")
+  inhib <- max(pred_probs)-((x/100) * max(pred_probs))
   library(VGAM)
-  eta <- logit(y)
-  beta <- coef(x)[1:2]
-  ecx <- (eta - beta[1]) / beta[2]
-  pd <- -cbind(1, ecx) / beta[2]
-  ff <- as.matrix(vcov(x)[1:2, 1:2])
+  eta <- logitlink(inhib)
+  beta <- coef(model)[1:2]
+  ECx <- (eta - beta[1]) / beta[2]
+  pd <- -cbind(1, ECx) / beta[2]
+  ff <- as.matrix(vcov(model)[1:2, 1:2])
   se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
-  upper <- (ecx + se * 1.96)
-  lower <- (ecx - se * 1.96)
-  sum <- data.frame(ecx, lower, upper)
+  upper <- (ECx + se * 1.96)
+  lower <- (ECx - se * 1.96)
+  sum <- data.frame(ECx, lower, upper)
   return(sum)
 } # using coef
