cfce99ac3e6350a2437c3f0f9efbec10db8b3818
Gerard Ricardo <g.ricado@aims.gov.au>
2024-03-12 14:16:16 +1000

    added gaussian

--- R/ECx.R
+++ R/ECx.R
@@ -7,58 +7,115 @@
 #' @param model A model object, typically from `glm` or `glmer` from the `lme4` package, or `glmmTMB`.
 #' @param x The percentage effect to calculate the ECx at (default is 50).
 #' @param type The type of ECx calculation: 'from_top' (default), or 'from_top_bot' or 'absolute'. 'from_top' calculates the ECx from
-#' the maximum of the model prediction, 'from_top_bot' between the range of the maximum and minimum of the model prediction, and 'absolute'
-#' is used to directly calculate the ECx at an absolute value.
+#' the maximum of the model prediction to zero, 'from_top_bot' between the range of the maximum and minimum of the model prediction, and 'absolute'
+#' is used to directly calculate the ECx at an absolute value.Which is most appropriate depends on the specific research question
 #' @return A data frame with ECx, lower, and upper confidence limits.
 #' @importFrom lme4 fixef
 #' @importFrom VGAM logitlink
 #' @importFrom glmmTMB fixef
-#' @importFrom stats vcov coef predict
+#' @importFrom stats vcov coef predict family
 #' @export
 #' @examples
 #' set.seed(123)
-#'raw_x <- sort(rep(seq(from = 0.1, to = 100, by = 10), 4))
-#'suc <- rbinom(n = length(raw_x), size = 100, prob = raw_x/100)
-#'tot <- rep(100, length(raw_x))
-#'data1 <- data.frame(raw_x, suc, tot)
-#'md1 <- glm(cbind(suc, (tot - suc)) ~ raw_x, family = binomial, data = data1)
-#'ECx(model = md1, x = 50)
+#' raw_x <- sort(rep(seq(from = 0.1, to = 100, by = 10), 4))
+#' suc <- 100 - rbinom(n = length(raw_x), size = 100, prob = raw_x / 100)
+#' tot <- rep(100, length(raw_x))
+#' data1 <- data.frame(raw_x, suc, tot)
+#' md1 <- glm(cbind(suc, (tot - suc)) ~ raw_x, family = binomial, data = data1)
+#' ECx(model = md1, x = 50)
 #'
-ECx <- function(model, x = 50, type = c('from_top', 'from_top_bot', 'absolute')) {
+ECx <- function(model, x = 50, type = c("from_top", "from_top_bot", "absolute")) {
+  # Extract model family as a string
+
+
+
+  # type of ECx
   type <- match.arg(type)
-
-  # Check if the model is a GLMM from lme4 or glmmTMB
-  if("glmerMod" %in% class(model)) {
-    beta <- fixef(model)  # For lme4 GLMMs, use fixef() to get fixed effects
-    ff <- as.matrix(vcov(model)[1:2, 1:2])
-  } else if ("glmmTMB" %in% class(model)) {
-    beta <- fixef(model)$cond  # For glmmTMB, use fixef(model)$cond
-    ff <- as.matrix(vcov(model)$cond[1:2, 1:2])
-  } else {
-    beta <- coef(model)  # For GLMs and other models, use coef()
-    ff <- as.matrix(vcov(model)[1:2, 1:2])
-  }
-
   pred_probs <- predict(model, type = "response")
-
-  if (type == 'from_top') {
-    inhib <- max(pred_probs) - ((x/100) * max(pred_probs))
-  } else if (type == 'from_top_bot') {
+  if (type == "from_top") {
+    inhib <- max(pred_probs) - ((x / 100) * max(pred_probs))
+  } else if (type == "from_top_bot") {
     range_val <- max(pred_probs) - min(pred_probs)
-    target_val <- min(pred_probs) + (range_val * (x/100))
+    target_val <- min(pred_probs) + (range_val * (x / 100))
     inhib <- target_val
-  } else if (type == 'absolute') {
-    inhib <- x/100
+  } else if (type == "absolute") {
+    inhib <- x / 100
   }
 
-  eta <- VGAM::logitlink(inhib)
-  #beta <- coef(model)[1:2]
-  ECx <- (eta - beta[1]) / beta[2]
-  pd <- -cbind(1, ECx) / beta[2]
-  #ff <- as.matrix(vcov(model)[1:2, 1:2])
-  se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
-  upper <- (ECx + se * 1.96)
-  lower <- (ECx - se * 1.96)
-  sum <- data.frame(ECx, lower, upper)
-  return(sum)
+  # Check model package and family
+  model_family <- family(model)$family
+  if ("glmerMod" %in% class(model) & model_family == "binomial") {
+    beta <- fixef(model) # For lme4 GLMMs
+    eta <- VGAM::logitlink(inhib)
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  } else if ("glmerMod" %in% class(model) & model_family == "gaussian") {
+    beta <- fixef(model) # For glmmTMB
+    eta <- inhib
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  } else if ("glmmTMB" %in% class(model) & model_family == "binomial") {
+    beta <- fixef(model)$cond # For glmmTMB
+    eta <- VGAM::logitlink(inhib)
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)$cond[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  } else if ("glmmTMB" %in% class(model) & model_family == "gaussian") {
+    beta <- fixef(model)$cond # For glmmTMB
+    eta <- inhib
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)$cond[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  } else if ("glm" %in% class(model) & model_family == "binomial") {
+    beta <- coef(model) # For lme4 GLMMs
+    eta <- VGAM::logitlink(inhib)
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  } else if ("glm" %in% class(model) & model_family == "gaussian") {
+    beta <- coef(model) # For lme4 GLMMs
+    eta <- inhib
+    ECx <- (eta - beta[1]) / beta[2]
+    pd <- -cbind(1, ECx) / beta[2]
+    ff <- vcov(model)[1:2, 1:2]
+    se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+    upper <- (ECx + se * 1.96)
+    lower <- (ECx - se * 1.96)
+    sum <- data.frame(ECx, lower, upper)
+  }
+
+
+
+  # eta <- VGAM::logitlink(inhib)
+  # # beta <- coef(model)[1:2]
+  # ECx <- (eta - beta[1]) / beta[2]
+  # pd <- -cbind(1, ECx) / beta[2]
+  # # ff <- as.matrix(vcov(model)[1:2, 1:2])
+  # se <- sqrt(((pd %*% ff) * pd) %*% c(1, 1))
+  # upper <- (ECx + se * 1.96)
+  # lower <- (ECx - se * 1.96)
+  # sum <- data.frame(ECx, lower, upper)
+   return(sum)
 }
--- tests/testthat/test-ECx.R
+++ tests/testthat/test-ECx.R
@@ -2,7 +2,7 @@ test_that("Description of the test", {
   # Preparation code (if needed)
   set.seed(123)
   raw_x <- sort(rep(seq(from = 0.1, to = 100, by = 10), 4))
-  suc <- rbinom(n = length(raw_x), size = 100, prob = raw_x/100)
+  suc <- 100 - rbinom(n = length(raw_x), size = 100, prob = raw_x/100)
   tot <- rep(100, length(raw_x))
   data1 <- data.frame(raw_x, suc, tot)
   md1 <- glm(cbind(suc,(tot - suc)) ~ raw_x, family = binomial, data = data1)
